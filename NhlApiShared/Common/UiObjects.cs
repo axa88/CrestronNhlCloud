2bc8909c3119ed3f4571b8b1bad549e1a0ae0c64
Work <axa88@yahoo.com>
2021-02-28 02:42:33 -0500

    sync

--- NhlApiShared/Common/UiObjects.cs
+++ NhlApiShared/Common/UiObjects.cs
@@ -2,7 +2,6 @@
 {
 	public static class UiObjects
 	{
-		//public const string DefaultTeamKey = nameof(DefaultTeamKey);
 		public const string TileStatus = nameof(TileStatus);
 		public const string TileIcon = nameof(TileIcon);
 		public const string TileSecondaryIcon = nameof(TileSecondaryIcon);
